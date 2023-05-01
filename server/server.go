f89d109c88c8b975b81231e83234830e49bbc609
alisalim17 <alisalimli1899@gmail.com>
2023-05-01 22:49:28 +0400

    refactor folder structure

--- server/server.go
+++ server/server.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
@@ -15,9 +13,6 @@ import (
 	"github.com/elisalimli/go_graphql_template/domain"
 	"github.com/elisalimli/go_graphql_template/graphql"
 	"github.com/elisalimli/go_graphql_template/initializers"
-	"github.com/elisalimli/go_graphql_template/migrations"
-	"github.com/uptrace/bun/migrate"
-	"github.com/urfave/cli/v2"
 
 	customMiddleware "github.com/elisalimli/go_graphql_template/middleware"
 	"github.com/elisalimli/go_graphql_template/postgres"
@@ -25,140 +20,6 @@ import (
 	"github.com/go-chi/chi/middleware"
 	"github.com/rs/cors"
 )
-
-var Migrations = migrate.NewMigrations()
-
-func newDBCommand(migrator *migrate.Migrator) *cli.Command {
-	return &cli.Command{
-		Name:  "db",
-		Usage: "database migrations",
-		Subcommands: []*cli.Command{
-			{
-				Name:  "init",
-				Usage: "create migration tables",
-				Action: func(c *cli.Context) error {
-					return migrator.Init(c.Context)
-				},
-			},
-			{
-				Name:  "migrate",
-				Usage: "migrate database",
-				Action: func(c *cli.Context) error {
-					if err := migrator.Lock(c.Context); err != nil {
-						return err
-					}
-					defer migrator.Unlock(c.Context) //nolint:errcheck
-
-					group, err := migrator.Migrate(c.Context)
-					if err != nil {
-						return err
-					}
-					if group.IsZero() {
-						fmt.Printf("there are no new migrations to run (database is up to date)\n")
-						return nil
-					}
-					fmt.Printf("migrated to %s\n", group)
-					return nil
-				},
-			},
-			{
-				Name:  "rollback",
-				Usage: "rollback the last migration group",
-				Action: func(c *cli.Context) error {
-					if err := migrator.Lock(c.Context); err != nil {
-						return err
-					}
-					defer migrator.Unlock(c.Context) //nolint:errcheck
-
-					group, err := migrator.Rollback(c.Context)
-					if err != nil {
-						return err
-					}
-					if group.IsZero() {
-						fmt.Printf("there are no groups to roll back\n")
-						return nil
-					}
-					fmt.Printf("rolled back %s\n", group)
-					return nil
-				},
-			},
-			{
-				Name:  "lock",
-				Usage: "lock migrations",
-				Action: func(c *cli.Context) error {
-					return migrator.Lock(c.Context)
-				},
-			},
-			{
-				Name:  "unlock",
-				Usage: "unlock migrations",
-				Action: func(c *cli.Context) error {
-					return migrator.Unlock(c.Context)
-				},
-			},
-			{
-				Name:  "create_go",
-				Usage: "create Go migration",
-				Action: func(c *cli.Context) error {
-					name := strings.Join(c.Args().Slice(), "_")
-					mf, err := migrator.CreateGoMigration(c.Context, name)
-					if err != nil {
-						return err
-					}
-					fmt.Printf("created migration %s (%s)\n", mf.Name, mf.Path)
-					return nil
-				},
-			},
-			{
-				Name:  "create_sql",
-				Usage: "create up and down SQL migrations",
-				Action: func(c *cli.Context) error {
-					name := strings.Join(c.Args().Slice(), "_")
-					files, err := migrator.CreateSQLMigrations(c.Context, name)
-					if err != nil {
-						return err
-					}
-
-					for _, mf := range files {
-						fmt.Printf("created migration %s (%s)\n", mf.Name, mf.Path)
-					}
-
-					return nil
-				},
-			},
-			{
-				Name:  "status",
-				Usage: "print migrations status",
-				Action: func(c *cli.Context) error {
-					ms, err := migrator.MigrationsWithStatus(c.Context)
-					if err != nil {
-						return err
-					}
-					fmt.Printf("migrations: %s\n", ms)
-					fmt.Printf("unapplied migrations: %s\n", ms.Unapplied())
-					fmt.Printf("last migration group: %s\n", ms.LastGroup())
-					return nil
-				},
-			},
-			{
-				Name:  "mark_applied",
-				Usage: "mark migrations as applied without actually running them",
-				Action: func(c *cli.Context) error {
-					group, err := migrator.Migrate(c.Context, migrate.WithNopMigration())
-					if err != nil {
-						return err
-					}
-					if group.IsZero() {
-						fmt.Printf("there are no new migrations to mark as applied\n")
-						return nil
-					}
-					fmt.Printf("marked as applied %s\n", group)
-					return nil
-				},
-			},
-		},
-	}
-}
 
 func init() {
 	initializers.LoadEnvVariables()
@@ -177,16 +38,6 @@ const defaultPort = "4000"
 
 func main() {
 
-	app := &cli.App{
-		Name: "bun",
-
-		Commands: []*cli.Command{
-			newDBCommand(migrate.NewMigrator(initializers.DB, migrations.Migrations)),
-		},
-	}
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
 	var mb int64 = 1 << 20
 
 	port := os.Getenv("PORT")
