caa20f3d3a3de7f102e62508ff951356fe2cfcc9
alisalim17 <alisalimli1899@gmail.com>
2023-05-15 16:38:53 +0400

    fix

--- myapp/app.config.js
+++ myapp/app.config.js
@@ -30,7 +30,7 @@ module.exports = {
     apiUrl: process.env.GOOGLE_IOS_CLIENT_ID,
     // ENTRY_FILE: "index.ts",
     eas: {
-      projectId: "5b5f57ad-f194-4ef0-8bd6-7bf559174171",
+      projectId: "28d50f92-9b57-4839-a174-715217aeb8c8",
     },
   },
 };
