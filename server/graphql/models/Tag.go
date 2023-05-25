40fa8f96fda2b70fb8ea8ce01829d4e45e745720
alisalim17 <alisalimli1899@gmail.com>
2023-05-25 19:47:21 +0400

    adding tags to seed data

--- server/graphql/models/Tag.go
+++ server/graphql/models/Tag.go
@@ -1,7 +1,8 @@
 package models
 
 type Tag struct {
-	Id   string `bun:"type:uuid,default:uuid_generate_v4(),pk"`
-	Name string
-	Code string
+	Id        string `bun:"type:uuid,default:uuid_generate_v4(),pk"`
+	Name      string
+	Code      string
+	CatalogId string `bun:"catalog_id;type:uuid"`
 }
