fa066d3d2bb4fe4a240ab6f3d96ba0950332ad0b
alisalim17 <alisalimli1899@gmail.com>
2023-06-05 14:05:03 +0400

    joining catalogs in tags query

--- server/graphql/tag.resolver.go
+++ server/graphql/tag.resolver.go
@@ -11,7 +11,9 @@ import (
 
 func (r *queryResolver) Tags(ctx context.Context) ([]*models.Tag, error) {
 	tags := make([]*models.Tag, 0)
-	q := r.Domain.PostsRepo.DB.NewSelect().Model(&tags).Column(`tag.*`)
+	q := r.Domain.PostsRepo.DB.NewSelect().Model(&tags).Column(`tag.*`).
+		ColumnExpr("json_build_object('id', c.id,'name', c.name,'code', c.code) as catalog").
+		Join("LEFT JOIN catalogs c ON tag.catalog_id = c.id")
 
 	err := q.Scan(ctx)
 
